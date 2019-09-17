990866d1419a20c65e9aba1f58e9b72d57be8c5f
LzeLlWT <40066683+LzeLlWT@users.noreply.github.com>
2019-09-17 17:32:29 +0800

    TodoList

--- src/pages/Page1.vue
+++ src/pages/Page1.vue
@@ -33,14 +33,4 @@
         top :0;
         bottom: 0;
     }
-    #cover{
-        position: absolute;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #999;
-        opacity :.9;
-        z-index: -1;
-    }
 </style>
